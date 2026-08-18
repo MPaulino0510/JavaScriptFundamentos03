43222db875be51ca4a5b89011bde28592ae60f7a
MIGUEL PAULINO ALVES PEREIRA <miguelpaulino2009.ap@gmail.com>
2026-08-18 14:23:26 -0300

    Salvando

---
+++ 02-metodos arrays/04-find.js
@@ -0,0 +1,9 @@
+const alunos = ["Giovanni", "Gustavo", "Ingrid", "Isabela"];
+
+const aluno = alunos.find((nome) => nome === "Gustavo");
+
+console.log(aluno)
+
+const aluno1 = alunos.find((nome) => nome === "Felipe");
+
+console.log(aluno1);
