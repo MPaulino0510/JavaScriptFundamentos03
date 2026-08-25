514db9ef3668a7b05a40bbfc7c6da5c70eabb1b7
MIGUEL PAULINO ALVES PEREIRA <miguelpaulino2009.ap@gmail.com>
2026-08-25 09:26:17 -0300

    Salvando

---
+++ 06-introducao-poo/03-instancias.js
@@ -0,0 +1,10 @@
+class Pessoa {
+    constructor(nome, idade, ){
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
+
+const pessoa = new Pessoa("Miguel", 16);
+const pessoa1 = new Pessoa("Arthur", 17);
+console.log(pessoa, pessoa1);
