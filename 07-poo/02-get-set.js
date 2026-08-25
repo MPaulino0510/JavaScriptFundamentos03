b0a179488969c794b5719ccc82a8aad021df086f
MIGUEL PAULINO ALVES PEREIRA <miguelpaulino2009.ap@gmail.com>
2026-08-25 10:37:54 -0300

    Salvando

---
+++ 07-poo/02-get-set.js
@@ -0,0 +1,23 @@
+class Pessoa{
+    #nome;
+
+    constructor(nome){
+    this.#nome = nome;
+    }
+
+    get nome(){
+        return this.#nome;
+    }
+
+    set nome(valor){
+        if(valor.lenght >= 2)
+             this.#nome = valor;
+            
+    }
+}
+
+
+const pessoa = new Pessoa("Miguel");
+console.log(pessoa.nome);
+pessoa.nome = "Victória";
+console.log(pessoa.nome);
---
+++ 07-poo/04-super.js
@@ -0,0 +1,17 @@
+class Funcionario{
+    constructor(nome, salario){
+        this.nome = nome;
+        this.salario = salario;
+    }
+}
+
+class Professor extends Funcionario{
+    constructor(nome, salario, disciplina){
+        super(nome, salario);
+        this.disciplina = disciplina;
+    }
+}
+
+const professor = new Professor("Marcos", 5000, "T.I")
+
+console.log(professor);
