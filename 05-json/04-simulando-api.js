a17c4dbe5989ffdb64d5afca7c4f282ee19840f3
MIGUEL PAULINO ALVES PEREIRA <miguelpaulino2009.ap@gmail.com>
2026-08-25 09:08:00 -0300

    Salvando

---
+++ 05-json/04-simulando-api.js
@@ -0,0 +1,10 @@
+const respostaAPI = `[
+    {"id":1, "nome": "Miguel"},
+    {"id":2, "nome": "Arthur"}
+]`;
+
+const usuarios = JSON.parse(respostaAPI);
+
+usuarios.forEach(aluno => {
+    console.log(aluno.nome);
+});
